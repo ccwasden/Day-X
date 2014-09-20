7e7cb8ce1c2796272e43011f6f52dfd9951a6a9b
Zack Lounsbury <zack.lounsbury@gmail.com>
2014-09-20 11:30:06 -0600

    Updates to detailView

--- DayX/DetailViewController.m
+++ DayX/DetailViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "DetailViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+static NSString *entryKey = @"entry";
+static NSString *titleKey = @"title";
+static NSString *textKey = @"text";
 
 @interface DetailViewController () <UITextFieldDelegate, UITextViewDelegate>
 @property (strong, nonatomic) IBOutlet UITextField *textField;
@@ -24,12 +29,68 @@
     
     self.textField.delegate = self;
     self.textField.placeholder = @"Title";
+    
+    self.textView.delegate = self;
+    
+    //self.textView.text = @"Notes...";
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.textView.layer.borderWidth = 1.0f;
+    self.textView.layer.borderColor = [[UIColor grayColor] CGColor];
+    self.textView.layer.cornerRadius = 8;
+    
+    NSMutableDictionary *entry = [[NSUserDefaults standardUserDefaults] valueForKey:entryKey];
+    
+    [self updateWithDictionary:entry];
+    
 }
 
 - (IBAction)clearButton:(id)sender {
     
     self.textField.text = @"";
     self.textView.text = @"";
+   
+}
+
+- (void)updateWithDictionary:(NSDictionary *)dictionary {
+    
+    NSString *title = [dictionary objectForKey:titleKey];
+    
+    if (title) {
+        self.textField.text = title;
+    }
+    
+    NSString *text = [dictionary objectForKey:textKey];
+    
+    if (text) {
+        self.textView.text = text;
+    }
+    
+}
+
+- (void)save {
+    
+    NSMutableDictionary *data = [NSMutableDictionary new];
+    
+    [data setObject:self.textField.text forKey:titleKey];
+    [data setObject:self.textView.text forKey:textKey];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:data forKey:entryKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    
+    
+    
+    
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    
+    [self save];
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    
+    [self save];
 }
 
 
